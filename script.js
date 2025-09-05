dbc6acda50ff975220e3b1f58b669d7565a9b9c7
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-09-05 15:33:59 +0200

    completamento non estetico

--- script.js
+++ script.js
@@ -5,19 +5,19 @@
     
     const km = parseInt(document.getElementById('km').value);
     const eta = parseInt(document.getElementById('eta').value);
-    let risultato = document.getElementById('risultato')
+    let risultato = document.getElementById('risultato');
 
     // Calcolo del prezzo base
     let prezzoBase = km * 0.21;
 
     // Applicazione degli sconti
     if (eta < 18) {
-        prezzoBase *= 0.8; // Sconto del 20%
+        prezzoBase = prezzoBase * 0.8; // Sconto del 20%
     } else if (eta >= 65) {
-        prezzoBase *= 0.6; // Sconto del 40%
-    }
+        prezzoBase = prezzoBase * 0.6; // Sconto del 40%
+    } 
 
-    risultato.textContent = 'Il risultato è: ', prezzoBase;
+    risultato.textContent = 'Il risultato è: ' + prezzoBase.toFixed(2) + '€';
 });
 
     
